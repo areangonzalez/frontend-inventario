0c8447c0292931badd49f5d0273d996a3dbeb6ec
areangonzalez <areangonzalez@gmail.com>
2020-06-03 14:04:17 -0300

    TG-44 Closed se crea el componente de sub menu

--- src/app/productos/productos.module.ts
+++ src/app/productos/productos.module.ts
@@ -4,11 +4,15 @@ import { ProductosComponent } from './productos.component';
 import { ListaComponent } from './lista/lista.component';
 import { ProductosRoutingModule } from './productos-routing.module';
 import { SharedModule } from '../shared';
+import { MenuComponent } from './menu/menu.component';
+import { StockComponent } from './menu/vista/stock/stock.component';
+import { IngresoComponent } from './menu/vista/ingreso/ingreso.component';
+import { EgresoComponent } from './menu/vista/egreso/egreso.component';
 
 
 
 @NgModule({
-  declarations: [ProductosComponent, ListaComponent],
+  declarations: [ProductosComponent, ListaComponent, MenuComponent, StockComponent, IngresoComponent, EgresoComponent],
   imports: [
     CommonModule,
     SharedModule,
