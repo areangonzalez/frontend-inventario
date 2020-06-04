992098ef107f2a7c661a0d7f23e664436b4f5011
areangonzalez <areangonzalez@gmail.com>
2020-06-04 13:16:57 -0300

    TG-45 se configura las rutas para los listados de stock - ingreso - egreso

--- src/app/productos/productos-routing.module.ts
+++ src/app/productos/productos-routing.module.ts
@@ -1,13 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductosComponent } from './productos.component';
+import { StockComponent } from './menu/vista/stock/stock.component';
+import { IngresoComponent } from './menu/vista/ingreso/ingreso.component';
+import { EgresoComponent } from './menu/vista/egreso/egreso.component';
 
 const routes: Routes = [
   {
     path: '',
     //data: {title: 'Productos'},
-    component: ProductosComponent
-  }
+    component: ProductosComponent,
+    children: [
+      {
+        path: 'stock', component: StockComponent
+      },
+      {
+        path: 'ingreso', component: IngresoComponent
+      },
+      {
+        path: 'egreso', component: EgresoComponent
+      },
+      { path: '**', redirectTo: 'stock', pathMatch: 'full' },
+    ]
+  },
 ]
 
 @NgModule({
