9e15d0def4292d540c5cc97a08cc6cd1e0d0579d
areangonzalez <areangonzalez@gmail.com>
2020-06-08 10:00:27 -0300

    se cambian los datos de los arrays

--- src/app/productos/menu/vista/stock/stock.component.ts
+++ src/app/productos/menu/vista/stock/stock.component.ts
@@ -8,12 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class StockComponent implements OnInit {
   public listadoStock: any = [
     { id:1, cantidad: 2000, producto: 'Aceite de girasol, 900 ml', marca: 'Legítimo', categoria: 'Alimentos / Bebidas', vencimiento: '2020-07-23' },
-    { id:1, cantidad: 3500, producto: 'Azucar blanca, 1 kg', marca: 'Ledesma', categoria: 'Alimentos / Bebidas', vencimiento: '2020-12-12' },
-    { id:1, cantidad: 4000, producto: 'Fideos secos guiseros, 500 gr', marca: 'Canale', categoria: 'Alimentos / Bebidas', vencimiento: '2020-08-12' },
-    { id:1, cantidad: 6000, producto: 'Leche entera, 1 lt', marca: 'Sancor', categoria: 'Alimentos / Bebidas', vencimiento: '2020-06-13' },
-    { id:1, cantidad: 1000, producto: 'Alcohol, 250 cc', marca: 'Purocol', categoria: 'Limpieza', vencimiento: '2022-03-20' },
-    { id:1, cantidad: 2500, producto: 'jabón blanco en pan, 200 gr', marca: 'Ala', categoria: 'Limpieza', vencimiento: '2021-03-20' },
-    { id:1, cantidad: 3000, producto: 'Papel hig. Hoja simple, 320 ml', marca: 'Higienol', categoria: 'Limpieza', vencimiento: '2025-06-25' }
+    { id:2, cantidad: 3500, producto: 'Azucar blanca, 1 kg', marca: 'Ledesma', categoria: 'Alimentos / Bebidas', vencimiento: '2020-12-12' },
+    { id:3, cantidad: 4000, producto: 'Fideos secos guiseros, 500 gr', marca: 'Canale', categoria: 'Alimentos / Bebidas', vencimiento: '2020-08-12' },
+    { id:4, cantidad: 6000, producto: 'Leche entera, 1 lt', marca: 'Sancor', categoria: 'Alimentos / Bebidas', vencimiento: '2020-06-13' },
+    { id:5, cantidad: 1000, producto: 'Alcohol, 250 cc', marca: 'Purocol', categoria: 'Limpieza', vencimiento: '2022-03-20' },
+    { id:6, cantidad: 2500, producto: 'jabón blanco en pan, 200 gr', marca: 'Ala', categoria: 'Limpieza', vencimiento: '2021-03-20' },
+    { id:7, cantidad: 3000, producto: 'Papel hig. Hoja simple, 320 ml', marca: 'Higienol', categoria: 'Limpieza', vencimiento: '2025-06-25' }
   ];
 
 
