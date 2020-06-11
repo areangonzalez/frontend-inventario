dbdebcee8d5984d7c5cc6c035b64040ee6f3ff13
areangonzalez <areangonzalez@gmail.com>
2020-06-11 12:19:23 -0300

    TG-88 #Closed se maqueta el formulario de busqueda avanzada para egreso

--- src/app/productos/menu/vista/busqueda-avanzada/ba-egreso.component.ts
+++ src/app/productos/menu/vista/busqueda-avanzada/ba-egreso.component.ts
@@ -1,15 +1,131 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { NgbDate, NgbDatepickerConfig, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
+import { UtilService } from 'src/app/core/service';
 
 @Component({
-  selector: 'app-ba-egreso',
+  selector: 'ba-egreso',
   templateUrl: './ba-egreso.component.html',
-  styleUrls: ['./ba-egreso.component.scss']
+  styleUrls: ['./ba-stock.component.scss']
 })
 export class BaEgresoComponent implements OnInit {
+  public categoriaLista: any = [{id:1, nombre:'Alimentos / Bebidas'}];
+  public marcaLista: any = [{id:1, nombre:'Canale'}];
+  public medidadLista: any = [{ id:1, nombre:'kg' },{ id:2, nombre:'lts' },{ id:3, nombre:'gr' }];
+  public globalParam:string = '';
+  public busquedaAvanzada: FormGroup;
+  public mostrar: boolean = false;
+  public btnSeleccion: boolean = false;
 
-  constructor() { }
+
+  // Variables para calendarios
+  public hoveredDate: NgbDate | null = null; // Resalta la fecha
+  public fromDate: NgbDate | null = null; // fecha desde
+  public toDate: NgbDate | null = null; // fecha hasta
+  public mostrarDp: boolean = false; // Muestra el DatePicker
+
+
+    constructor(
+      private _fb: FormBuilder,
+      private _configNgbDate: NgbDatepickerConfig,
+      private _calendar: NgbCalendar,
+      private _util: UtilService
+    ) {
+      // configuracion de fecha minima
+      _configNgbDate.minDate = {year: 1900, month: 1, day: 1};
+      // formulario de busqueda avanzada
+      this.busquedaAvanzada = _fb.group({
+        global_param: '',
+        marcaid: '',
+        categoriaid: '',
+        egresoDesde: null,
+        egreso_desde: '',
+        egresoHasta: null,
+        egreso_hasta: ''
+      });
+    }
 
   ngOnInit(): void {
+  }
+  public buscar(){
+    console.log('buscamos');
+  }
+
+  public limpiarCampos() {
+    console.log('limpiamos');
+  }
+  /**
+   * Formatea la fecha para una variable del formulario
+   * @param obj [any] objecto de fecha
+   * @param keyFecha [string] nombre de la variable que sera seteada
+   */
+  public formatFecha(obj:any, keyFecha:string){
+    if (obj != null){
+      this.busquedaAvanzada.controls[keyFecha].setValue(this._util.formatearFecha(obj.day, obj.month, obj.year, "yyyy-MM-dd"));
+    }else{
+      this.busquedaAvanzada.controls[keyFecha].setValue('');
+    }
+  }
+  /**
+   * Marca los campos que seran utilizados en la busqueda avanzada
+   * @param valor contiene el valor del input seleccionado
+   */
+  marcarCampo(valor: any){
+    let marcar:boolean = false;
+    marcar = (valor != null && valor != '') ? true : false;
+    return marcar;
+  }
+  /**
+   * Muestra/Oculta los campos de busqueda avanzada
+   */
+  public mostrarBusquedaAvanzada(){
+    return this.mostrar = !this.mostrar;
+  }
+  /**
+   * Valido si los valores ingresados al input son numeros,
+   * si no borro los valores ingresados
+   * @param datos objeto que contiene el valor del input
+   */
+  validarNumero(datos: any){
+    console.log(datos.value)
+    if (!this._util.validarNumero(datos.value)) {
+      datos.value = datos.value.substring(0,datos.value.length - 1);
+      this.busquedaAvanzada.get("unidad").patchValue(datos.value);
+    }
+  }
+
+  /* ### DATE PICKER CONFIG ### */
+  /**
+   * Selecciona el rango de fecha DESDE/HASTA
+   * @param date [NgbDate] objeto de fecha del DatePicker
+   */
+  public onDateSelection(date: NgbDate) {
+    if (!this.fromDate && !this.toDate) {
+      this.fromDate = date;
+      console.log(date);
+      this.busquedaAvanzada.patchValue({egresoDesde: date});
+    } else if (this.fromDate && !this.toDate && date.after(this.fromDate)) {
+      this.toDate = date;
+      this.busquedaAvanzada.patchValue({egresoHasta: date});
+      this.abrirDp();
+    } else {
+      this.toDate = null;
+      this.fromDate = date;
+      this.busquedaAvanzada.patchValue({egresoDesde: date});
+      this.busquedaAvanzada.patchValue({egresoHasta: null});
+    }
+  }
+  public isHovered(date: NgbDate) {
+    return this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate);
+  }
+  public isInside(date: NgbDate) {
+    return date.after(this.fromDate) && date.before(this.toDate);
+  }
+  public isRange(date: NgbDate) {
+    return date.equals(this.fromDate) || date.equals(this.toDate) || this.isInside(date) || this.isHovered(date);
+  }
+  public abrirDp(){
+    this.mostrarDp = !this.mostrarDp;
   }
 
 }
