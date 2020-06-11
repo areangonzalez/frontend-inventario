b3a32ecf7798c1857acab3335b26e4365aaf3511
areangonzalez <areangonzalez@gmail.com>
2020-06-11 14:28:23 -0300

    TG-78 se maqueta el formulario para agregar un comprobante de stock

--- src/app/shared/formulario/comprobante/comprobante.component.ts
+++ src/app/shared/formulario/comprobante/comprobante.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AlertService, UtilService } from 'src/app/core/service';
 
 @Component({
   selector: 'app-comprobante',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comprobante.component.scss']
 })
 export class ComprobanteComponent implements OnInit {
+  public comprobanteForm: FormGroup;
+  public nroComprobantePrincipal: string = '';
+  public nroComprobanteFinal: string = '';
 
-  constructor() { }
+  constructor(
+    private _fb: FormBuilder, private _mensajeService: AlertService,
+    private _util: UtilService
+  ) {
+    this.comprobanteForm = _fb.group({
+      nro_comprobante: '',
+      fecha_emision: '',
+      fechaEmision: ''
+    });
+  }
 
   ngOnInit(): void {
   }
+
+  validarNumeroComprobante(numero: number) {
+    console.log(numero);
+  }
+
+
+
 
 }
