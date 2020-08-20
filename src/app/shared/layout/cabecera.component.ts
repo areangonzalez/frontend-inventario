8e0470d48473c7f0a50ba98622b574b04bbd1615
areangonzalez <areangonzalez@gmail.com>
2020-08-20 12:48:41 -0300

    TG-382 #Closed se aplica funcionalidad para el cierre de sesion

--- src/app/shared/layout/cabecera.component.ts
+++ src/app/shared/layout/cabecera.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AutenticacionService, LoaderService } from 'src/app/core/service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'layout-cabecera',
@@ -6,13 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cabecera.component.scss']
 })
 export class CabeceraComponent implements OnInit {
+  public isCollapsed = true;
+  public mostrar: boolean = false;
+  public nombreUsuario: string = '';
+  public estoyLogueado:boolean = false;
 
-  constructor() { }
+  constructor( private _router: Router, private _autenticacion: AutenticacionService, private _loader: LoaderService ) { }
 
   ngOnInit(): void {
+    this.nombreUsuario = this._autenticacion.getUserName();
+    this.estoyLogueado = this._autenticacion.loggedIn();
   }
 
   cerrarSesion() {
-    console.log("cierre de sesion");
+    this._loader.show();
+      setTimeout(() => {
+        this._autenticacion.logout();
+        this._loader.hide();
+        this._router.navigate(['/login']);
+       }, 1000);
   }
+
+  mostrarMenu(){
+    this.mostrar = !this.mostrar;
+  }
+
+  ocultarMenu(){
+    this.mostrar = false;
+  }
+
+
 }
