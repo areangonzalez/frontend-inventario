2ed1dc483edd791c7c0d6145cbb0d863918d39f8
areangonzalez <areangonzalez@gmail.com>
2022-04-28 11:35:44 -0300

    se cambia el acceso del ambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://api.inventario.local/api'
+  baseUrl: 'http://inventario.local/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   // baseUrl: '/apimock'
-  baseUrl: 'http://api.inventario.local/api'
+  baseUrl: 'http://inventario.local/api'
 };
 
 /*
