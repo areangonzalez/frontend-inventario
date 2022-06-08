e9bbddaf8c454668033afe586e26b19b74306b09
cep11p <cep11p@gmail.com>
2022-06-08 11:56:43 -0300

    TG-601 - TG-604 #Closed se listan las marcas https://tree.taiga.io/project/areangonzalez-gestor-de-inventario/us/601

--- src/app/admin/componentes/lista/marca-lista.component.ts
+++ src/app/admin/componentes/lista/marca-lista.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AlertService, MarcaAbmService } from 'src/app/core/service';
+import { ConfiguracionListados } from './../../../core/model';
 
 @Component({
-  selector: 'app-marca-lista',
+  selector: 'admin-marca-lista',
   templateUrl: './marca-lista.component.html',
   styleUrls: ['./marca-lista.component.scss']
 })
 export class MarcaListaComponent implements OnInit {
+  @Input("listados") public listados: ConfiguracionListados;
+  @Input("configPaginacion") public configPaginacion: any;
+  @Output("cambioDePagina") public cambioDePagina = new EventEmitter();
+  @Output("cambioDeTamanioPagina") public cambioDeTamanioPagina = new EventEmitter();
+  public tamanioPagina: number = 20;
 
-  constructor() { }
+  constructor(private _msj: AlertService, private _marcaAbmService: MarcaAbmService) { }
 
   ngOnInit(): void {
+  }
+
+  infoAdicional(activo: number) {
+    return ( activo == 1 )? 'Activo' : 'Inactivo';
+  }
+
+  /**
+   * Envio al componente padre el numero de pagina
+   * @param pagina numero de pagina
+   */
+   cambioPagina(pagina:number){
+    this.cambioDePagina.emit(pagina);
+  }
+
+  cambioTamanioPagina(size: number) {
+    this.tamanioPagina = size;
+    this.cambioDeTamanioPagina.emit(size);
+  }
+
+  inactivarProveedor(confirmacion: boolean, activo:number, marcaid: number) {
+    if (confirmacion) {
+      let param:any = { activo: (activo == 1) ? 0 : 1};
+      this._marcaAbmService.altaBajaMarca(marcaid, param).subscribe(
+        resultado => {
+          if (param.activo == 0) {
+            this._msj.exitoso("La marca a sido dado de baja correctamente.");
+          }else{
+            this._msj.exitoso("La marca a sido dado de alta correctamente.");
+          }
+          this.cambioPagina(this.configPaginacion.page);
+        }, error => { this._msj.cancelado(error); })
+    }
   }
 
 }
